e7086e67c44832e693f849c9f59d8b8bbd32d496
Victor Chen <yvictorck@gmail.com>
2018-02-22 21:56:09 -0800

    a

--- shmlib.c
+++ shmlib.c
@@ -239,13 +239,12 @@ hb = (heartbeat_t*) shmat(shmid, NULL, 0);
 }
 int anchors_heartbeat_finish(int hb_shm_id) {
     int shmid;
-        printf("meow\n");
 
-  if ((shmid = shmget(hb_shm_id, 27, 0666)) < 0) {
+  if ((shmid = shmget(hb_shm_id, 1*sizeof(heartbeat_t), 0666)) < 0) {
         perror("shmget");
-        printf("no hb\n");
         return 0;
     }
+
 
   heartbeat_t* hb = (heartbeat_t*) shmat(shmid, NULL, 0);
   if (hb != NULL) {
