9576b9511f5bc94e1e212c04d07f37242b1c4f2d
Victor Chen <yvictorck@gmail.com>
2018-02-22 22:11:40 -0800

    a

--- test_wtih_obj_track.py
+++ test_wtih_obj_track.py
@@ -6,6 +6,11 @@ import sys
 import time
 import _thread
 
+# for obj track
+import numpy as np
+import cv2
+from tkinter import *
+import numpy.fft as fft
 shmlib = cdll.LoadLibrary('./shmlib.so')
 pids = [1024]
 
@@ -67,16 +72,45 @@ result = None
 # 	print("nooo")
 # 	pass
 
+
+
+master = Tk()
+w1 = Scale(master,from_=0,to=400)
+w1.set(100)
+w1.pack()
+w2 = Scale(master,from_=0,to=400,orient=HORIZONTAL)
+w2.set(200)
+w2.pack()
+cap = cv2.VideoCapture('/root/drop.avi')
+ret,frame = cap.read()
+frame2 = np.zeros((frame.shape),dtype=frame.dtype)
+
 cnt=-1
-while cnt<3:
+while(True):
 	cnt+=1
-	time.sleep(1)
+	ret, frame = cap.read()
+	try:
+		g2 = abs(frame-frame2)
+	except:
+		break
+	cv2.imshow('frame',g2)
+	frame2=frame
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	master.update_idletasks()
+	master.update()
 	for i in range(lenn):
 		# p=logmem[i].read()
 		shmlib.anchors_heartbeat.restype = ctypes.c_int64
 		hbtime = shmlib.anchors_heartbeat(pids[i],cnt)
 		hr = shmlib.get_hr_from_hb(pids[i],cnt)/1e6
 		print('hbtime',hbtime,'hr',hr)
+
+
+
+	# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
 for i in range(lenn):
 	if shmlib.anchors_heartbeat_finish(pids[i]):
 		print("clean up",pids[i])
