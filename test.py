a99920eda6e509943909e21b6c4bf9987dd3fefd
Victor Chen <yvictorck@gmail.com>
2018-02-22 21:17:53 -0800

    a

--- test.py
+++ test.py
@@ -51,7 +51,9 @@ vic_log_file ="vic.log";
 vic_min_target = 100;
 vic_max_target = 1000;
 for i in range(lenn):
-	print(shmlib.anchors_heartbeat_init(hbids[i],pids[i],vic_win_size,vic_buf_depth,vic_log_file,vic_min_target,vic_max_target))
+	suc = shmlib.anchors_heartbeat_init(hbids[i],pids[i],vic_win_size,vic_buf_depth,vic_log_file,vic_min_target,vic_max_target))
+	if suc:
+		print("hb_init!")
 logmem = []
 glomem = []
 result = None
