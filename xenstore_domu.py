25b130758f3ad0ea71d452ef2078d23e63866366
Victor Chen <yvictorck@gmail.com>
2018-02-24 02:36:52 -0800

    a

--- xenstore_domu.py
+++ xenstore_domu.py
@@ -1,9 +1,12 @@
 from pyxs import Client
 with Client(xen_bus_path="/dev/xen/xenbus") as c:
-	print(c.get_perms(b"/local/domain/4"))
-	print(c.list(b"/local/domain/4"))
-	c.write(b'/local/domain/4/vic',b'heyy')
-	print(c.get_perms(b"/local/domain/4/vic"))
+	try:
+		print(c.get_perms(b"/local/domain/4"))
+		print(c.list(b"/local/domain/4"))
+		c.write(b'/local/domain/4/vic',b'heyy')
+		print(c.get_perms(b"/local/domain/4/vic"))
+	except:
+		print("nope")
 
 	# print(c.read(b'/local/domain/'))
 
