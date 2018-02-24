a78adcc956cd60585435115c6658b83faaa12546
Victor Chen <yvictorck@gmail.com>
2018-02-24 02:32:10 -0800

    a

--- xenstore_dom0.py
+++ xenstore_dom0.py
@@ -1,5 +1,5 @@
 from pyxs import Client
 with Client(xen_bus_path="/dev/xen/xenbus") as c:
 	c.write(b'/local/domain/4/vic',b'hey')
-	c.set_perms(b"/local/domain/4/vic",[b'n4',b'n0'])
+	c.set_perms(b"/local/domain/4/vic",[b'b4',b'b0'])
 
