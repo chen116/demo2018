86fbd42328fb3cdfd02341041b604063f99a2ba2
Victor Chen <yvictorck@gmail.com>
2018-02-23 20:31:50 -0800

    a

--- client.py
+++ client.py
@@ -1,26 +1,36 @@
 import socket
 import time
 def Main():
-        host = '10.1.1.3'
-        port = 5000
+    # tcp/ip
+    # host = '10.1.1.3'
+    # port = 5000
 
-        mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        mySocket.connect((host,port))
+    # mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # mySocket.connect((host,port))
 
-        message = input(" -> ")
+    # message = input(" -> ")
 
-        while message != 'q':
-                a=(time.time())
-                message+='ip'
-                mySocket.send(message.encode())
-                print(time.time()-a)
-                data = mySocket.recv(1024).decode()
+    # while message != 'q':
+    #         a=(time.time())
+    #         message+='ip'
+    #         mySocket.send(message.encode())
+    #         print(time.time()-a)
+    #         data = mySocket.recv(1024).decode()
 
-                print ('Received from server: ' + data)
+    #         print ('Received from server: ' + data)
 
-                message = input(" -> ")
+    #         message = input(" -> ")
 
-        mySocket.close()
+    # mySocket.close()
+    # UDP
+    UDP_IP = "10.1.1.3"
+    UDP_PORT = 5005
+    message = "udp started"
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
+    while message != 'q':
+        sock.sendto(message, (UDP_IP, UDP_PORT))
+        print("sending:",message)
+        message = input("->")
 
 if __name__ == '__main__':
     Main()
--- server.py
+++ server.py
@@ -1,26 +1,34 @@
 import socket
  
 def Main():
-    host = "10.1.1.3"
-    port = 5000
+    # tcp/ip
+    # host = "10.1.1.3"
+    # port = 5000
      
-    mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    mySocket.bind((host,port))
+    # mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # mySocket.bind((host,port))
      
-    mySocket.listen()
-    conn, addr = mySocket.accept()
-    print ("Connection from: " + str(addr))
+    # mySocket.listen(1)
+    # conn, addr = mySocket.accept()
+    # while True:
+    #         data = conn.recv(1024).decode()
+    #         if not data:
+    #                 break
+    #         print ("from connected  user: " + str(data))
+             
+    #         data = str(data).upper()
+    #         print ("sending: " + str(data))
+    #         conn.send(data.encode())
+             
+    # conn.close()
+    #UDP
+    UDP_IP = "10.1.1.3"
+    UDP_PORT = 5005 
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
+    sock.bind((UDP_IP, UDP_PORT)) 
     while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                    break
-            print ("from connected  user: " + str(data))
-             
-            data = str(data).upper()
-            print ("sending: " + str(data))
-            conn.send(data.encode())
-             
-    conn.close()
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print("received message:", data)    
      
 if __name__ == '__main__':
     Main()
