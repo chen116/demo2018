c4356f82b501afb598b7f08d5c86dba4afd9163b
Victor Chen <yvictorck@gmail.com>
2018-02-25 15:35:48 -0800

    a

--- xenstore_dom0.py
+++ xenstore_dom0.py
@@ -2,7 +2,7 @@ from pyxs import Client
 with Client(xen_bus_path="/dev/xen/xenbus") as c:
 	c.set_perms(b"/local/domain/4/vic3",[b'b4',b'b0'])
 with Client(unix_socket_path="/var/run/xenstored/socket_ro") as c:
-	# c.write(b'/local/domain/4/vic2',b'hey')
+	c.write(b'/local/domain/5/vic2',b'hey')
 	m = c.monitor()
 	m.watch(b"/local/domain/4/vic2", b"a unique token")
 	msg = ""
