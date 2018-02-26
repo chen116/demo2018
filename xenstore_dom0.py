23052bdb7e2d3132edc9a1cb0f153bdec167510b
Victor Chen <yvictorck@gmail.com>
2018-02-25 23:51:26 -0800

    a

--- xenstore_dom0.py
+++ xenstore_dom0.py
@@ -6,9 +6,9 @@ from pyxs import Client
 
 domu_ids = []
 keys = ["heart_rate"]
-keys_full_path = []
+base_path = '/local/domain'
 with Client(xen_bus_path="/dev/xen/xenbus") as c:
-	for x in c.list(b'/local/domain'):
+	for x in c.list(base_path.encode()):
 		domu_ids.append(x.decode())
 	domu_ids.pop(0)
 	for domuid in domu_ids:
@@ -16,23 +16,20 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 		permissions.append(('b'+'0').encode())
 		permissions.append(('b'+domuid).encode())
 		for key in keys:
-			tmp_key_path = '/local/domain/'+domuid+'/'+key
+			tmp_key_path = base_path+'/'+domuid+'/'+key
 			tmp_val = 'init'
 			c.write(tmp_key_path.encode(),tmp_val.encode())
 			c.set_perms(tmp_key_path,permissions)
 			print('created',key,'for dom',domuid)
 
-	# c.write(b'/local/domain/5/vic',b'init_key_vic')
-	# c.set_perms(b"/local/domain/5/vic",[b'b5',b'b0'])
-	# c.write(b'/local/domain/4/vic',b'init_key_vic')
-	# c.set_perms(b"/local/domain/4/vic",[b'b4',b'b0'])
+
 
 
 with Client(unix_socket_path="/var/run/xenstored/socket_ro") as c:
 	m = c.monitor()
 	for domuid in domu_ids:
 		for key in keys:
-			tmp_key_path = '/local/domain/'+domuid+'/'+key
+			tmp_key_path = base_path+'/'+domuid+'/'+key
 			m.watch(tmp_key_path.encode(),(key+' '+domuid).encode())
 			print('watching',key,'for dom',domuid)
 	num_done = 0
@@ -43,17 +40,20 @@ with Client(unix_socket_path="/var/run/xenstored/socket_ro") as c:
 		if msg=='q':
 			num_done+=1
 
-
-
-
-	# m = c.monitor()
-	# m.watch(b"/local/domain/4/vic", b"dom4")
-	# m.watch(b"/local/domain/5/vic", b"dom5")
-	# msg = ""
-	# while msg!='q':
-	# 	print("from",(next(m.wait()))[0])
-	# 	msg = c.read(b'/local/domain/4/vic').decode()
-	# 	print(msg)
+# with Client(xen_bus_path="/dev/xen/xenbus") as c:
+# 	c.write(b'/local/domain/5/vic',b'init_key_vic')
+# 	c.set_perms(b"/local/domain/5/vic",[b'b5',b'b0'])
+# 	c.write(b'/local/domain/4/vic',b'init_key_vic')
+# 	c.set_perms(b"/local/domain/4/vic",[b'b4',b'b0'])
+# with Client(unix_socket_path="/var/run/xenstored/socket_ro") as c:
+# 	m = c.monitor()
+# 	m.watch(b"/local/domain/4/vic", b"dom4")
+# 	m.watch(b"/local/domain/5/vic", b"dom5")
+# 	msg = ""
+# 	while msg!='q':
+# 		print("from",(next(m.wait()))[0])
+# 		msg = c.read(b'/local/domain/4/vic').decode()
+# 		print(msg)
 
 
 
--- xenstore_domu.py
+++ xenstore_domu.py
@@ -2,15 +2,18 @@ from pyxs import Client
 
 
 
+
+keys = ["heart_rate"]
+base_path = '/local/domain'
 with Client(xen_bus_path="/dev/xen/xenbus") as c:
 	domU_id = c.read("domid".encode())
-	path = "/local/domain/"+domU_id.decode()+'/vic'
-	path = path.encode()
+	for key in keys:
+		tmp_key_path = base_path+'/'+domU_id.decode()+'/'+key
+		tmp_key_path = tmp_key_path.encode()
 	msg= ""
 	while msg!='q':
 		msg=input('->')
-		c.write(path,msg.encode())
-		print(msg)
+		c.write(tmp_key_path,msg.encode())
 	# print(c.get_perms("/local/domain".encode()))
 	# print(c.list(b"/local/domain"))
 	# c.write(b'/local/domain/4/vic',b'heyy')
