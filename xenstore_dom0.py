520a3d66346304f94f04ae1b9fd43a6c53f9cec1
Victor Chen <yvictorck@gmail.com>
2018-02-25 16:43:23 -0800

    a

--- xenstore_dom0.py
+++ xenstore_dom0.py
@@ -11,11 +11,13 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 
 
 
-
 with Client(unix_socket_path="/var/run/xenstored/socket_ro") as c:
 	m = c.monitor()
 	m.watch(b"/local/domain/4/vic", b"a-unique-token")
 	msg = ""
+
+	r = c.router()
+	print(r.is_connected())
 
 	while msg!='q':
 		print((next(m.wait()))[0])
