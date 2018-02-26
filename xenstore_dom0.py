cd8a9e7de20f0dff0c5178117cfb169caf6371b4
Victor Chen <yvictorck@gmail.com>
2018-02-25 16:30:33 -0800

    a

--- xenstore_dom0.py
+++ xenstore_dom0.py
@@ -14,12 +14,12 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 
 with Client(unix_socket_path="/var/run/xenstored/socket_ro") as c:
 	m = c.monitor()
-	m.watch(b"/local/domain/4/vic2", b"a unique token")
+	m.watch(b"/local/domain/4/vic", b"a-unique-token")
 	msg = ""
 
 	while msg!='q':
 		print((next(m.wait()))[0])
-		msg = c.read(b'/local/domain/4/vic2').decode()
+		msg = c.read(b'/local/domain/4/vic').decode()
 		print(msg)
 
 	# c.set_perms(b"/local/domain/4/vic2",[b'b4',b'b0'])
