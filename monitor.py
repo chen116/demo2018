2cb24b55ff5e99bb01f5128fde4c2bee1524f890
Victor Chen <yvictorck@gmail.com>
2018-02-25 21:08:20 -0800

    a

--- monitor.py
+++ monitor.py
@@ -1,8 +1,12 @@
 
 import subprocess
 
-proc = subprocess.Popen(['xl','list'], stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True)#,cwd='./linpack')
+proc = subprocess.Popen(['xl','list'])#, stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True)#,cwd='./linpack')
 
-
+try:
+	outs, errs = proc.communicate(timeout=15)
+except TimeoutExpired:
+	proc.kill()
+	outs, errs = proc.communicate()
 # st=proc.stdout.read()
 # er=proc.stderr.read()
