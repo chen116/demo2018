c1187954cac46efcd9bfad7cd9138232e8229fba
Victor Chen <yvictorck@gmail.com>
2018-02-25 17:20:33 -0800

    a

--- xenstore_dom0.py
+++ xenstore_dom0.py
@@ -13,12 +13,18 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 
 
 router = Router(pyxs.connection.UnixSocketConnection())
+
+
+
+
 with Client(router=router) as c:
+	m = c.monitor()
+	m.watch(b"/local/domain/4/vic", b"a-unique-token")
 	print(c.router.is_connected)
+	c.router.subscribe(b"a-unique-token",m)
 
 
-with Router(pyxs.connection.UnixSocketConnection()) as r:
-	print(r.is_connected)
+
 # with Client(unix_socket_path="/var/run/xenstored/socket_ro") as c:
 # 	m = c.monitor()
 # 	m.watch(b"/local/domain/4/vic", b"a-unique-token")
