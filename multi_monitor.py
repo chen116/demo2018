2d775824bb5a25cc91555d877b1933324024dbe0
Victor Chen <yvictorck@gmail.com>
2018-02-28 05:10:13 -0800

    a

--- multi_monitor.py
+++ multi_monitor.py
@@ -35,15 +35,22 @@ def res_allo(heart_rate,thread_shared_data,domuid):
 
     if heart_rate<200:
         if thread_shared_data[domuid]['bud'] < 10000:
-            thread_shared_data[domuid]['bud']+=1000
-
-            # proc = subprocess.Popen(['xl','sched-rtds','-d',domuid,'-p','10000','-b',str(thread_shared_data[domuid]['bud'])])
+            thread_shared_data[domuid]['bud']+=100
+            proc = subprocess.Popen(['xl','sched-rtds','-d',domuid,'-p','10000','-b',str(thread_shared_data[domuid]['bud'])])
             # try:
             #     outs, errs = proc.communicate(timeout=15)
             # except TimeoutExpired:
             #     proc.kill()
             #     outs, errs = proc.communicate()
-
+    if heart_rate>400:
+        if thread_shared_data[domuid]['bud'] < 10000:
+            thread_shared_data[domuid]['bud']-=100
+            proc = subprocess.Popen(['xl','sched-rtds','-d',domuid,'-p','10000','-b',str(thread_shared_data[domuid]['bud'])])
+            # try:
+            #     outs, errs = proc.communicate(timeout=15)
+            # except TimeoutExpired:
+            #     proc.kill()
+            #     outs, errs = proc.communicate()
 
 
 
