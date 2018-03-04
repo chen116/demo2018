60bc26dfd7bc4297ce88f82b4e1283714858c9d4
Victor Chen <yvictorck@gmail.com>
2018-03-04 11:27:44 -0800

    a

--- multi_monitor.py
+++ multi_monitor.py
@@ -21,8 +21,7 @@ shared_data = xen_interface.get_global_info()
 
 def res_allo(mode,heart_rate,thread_shared_data,domuid):
 
-    thread_shared_data["0"][0]['pcpu']+=1
-    print(thread_shared_data["0"][0]['pcpu'])
+    return
     # https://xenbits.xen.org/docs/unstable/man/xl.1.html#SCHEDULER-SUBCOMMANDS
     # cpupool, vcpupin, rtds-budget,period, extratime, vcpu-list
     # https://wiki.xenproject.org/wiki/Tuning_Xen_for_Performance
