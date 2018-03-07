b85e4a36fce61a16decb87137871d0f29187e295
Victor Chen <yvictorck@gmail.com>
2018-03-07 09:25:25 -0800

    a

--- multi_monitor.py
+++ multi_monitor.py
@@ -26,17 +26,19 @@ def res_allo(mode,heart_rate,thread_shared_data,domuid):
 		if vcpu['pcpu']!=-1:
 			cur_b=int(vcpu['b'])
 
-	# if(heart_rate<8):
-	# 	cur_b+=100
-	# 	xen_interface.sched_rtds(domuid,10000,cur_b,[])
-	# if(heart_rate>10):
-	# 	cur_b-=100
-	# 	xen_interface.sched_rtds(domuid,10000,cur_b,[])
-	# myinfo = thread_shared_data[domuid]
-	# for vcpu in myinfo:
-	# 	if vcpu['pcpu']!=-1:
-	# 		vcpu['b']=cur_b
-	# 		print(vcpu)
+	if(heart_rate<30):
+		if cur_b<=9900:
+			cur_b+=100
+			xen_interface.sched_rtds(domuid,10000,cur_b,[])
+	if(heart_rate>35):
+		cur_b>=200:
+			cur_b-=100
+			xen_interface.sched_rtds(domuid,10000,cur_b,[])
+	myinfo = thread_shared_data[domuid]
+	for vcpu in myinfo:
+		if vcpu['pcpu']!=-1:
+			vcpu['b']=cur_b
+			print(vcpu)
 
 
 	# xen_interface.update_domu_info(thread_shared_data,domuid)
