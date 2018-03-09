578d9427400dc31665f023366dd5214498e625f6
Victor Chen <yvictorck@gmail.com>
2018-03-09 01:50:57 -0800

    a

--- tf_track_vid_file_single.py
+++ tf_track_vid_file_single.py
@@ -6,6 +6,7 @@ import multiprocessing
 import numpy as np
 import tensorflow as tf
 import imutils
+from imutils.video import VideoStream
 
 from imutils.video import FileVideoStream
 from utils.app_utils import FPS, WebcamVideoStream
