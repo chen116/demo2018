63cc8a02407ac4404c048d8af87ab6df3c316047
Victor Chen <yvictorck@gmail.com>
2018-03-09 12:08:48 -0800

    a

--- turn_cam.py
+++ turn_cam.py
@@ -94,75 +94,77 @@ net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt", "MobileNetSSD
 # and initialize the FPS counter
 print("[INFO] starting video stream...")
 # vs = VideoStream('rtsp://arittenbach:8mmhamcgt16!@65.114.169.154:88/videoMain').start()
-# vs = VideoStream('rtsp://admin:admin@65.114.169.108:88/videoMain').start()
-vs= FileVideoStream("walkcat.mp4").start()
+vs = VideoStream('rtsp://admin:admin@65.114.169.108:88/videoMain').start()
+# vs= FileVideoStream("walkcat.mp4").start()
 
 time.sleep(2.0)
 fps = FPS().start()
 pointat = 0
 # loop over the frames from the video stream
-# while True:
-while vs.more():
+every_n_frame = 0
+while True:
+# while vs.more():
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
+	every_n_frame+=1
 	frame = vs.read()
 	current_f_size=w1.get()
 	if current_f_size == 0:
 		break
 	frame = imutils.resize(frame, width=current_f_size)
 
-
+	if every_n_frame%5==0:
 	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
-#	print(w)
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
-	#print(frame.dtype)
-	# loop over the detections
-	for i in np.arange(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the prediction
-		confidence = detections[0, 0, i, 2]
-		idx2 = int(detections[0,0,i,1])
-		# filter out weak detections by ensuring the `confidence` is
-		# greater than the minimum confidence
-		if ((confidence > args["confidence"]) and (CLASSES[idx2]=='cat')):
-			# extract the index of the class label from the
-			# `detections`, then compute the (x, y)-coordinates of
-			# the bounding box for the object
-			idx = int(detections[0, 0, i, 1])
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
-		#	print('startX=',startX)
-		#	print('endX=',endX)
-			if(((startX+endX)/2<(L*w)) and (moveleft==0)):
-				mycam.ptz_move_left()
-				moveleft = 1
-				moveright = 0
-			#	canpoint = 0
-			#	pointat = time.time()+0.3 
-			elif(((startX+endX)/2>(R*w)) and (moveright==0)):
-				mycam.ptz_move_right()
-				moveright = 1
-				moveleft = 0
-			#	canpoint = 0
-			#	pointat = time.time()+0.3
-			# draw the prediction on the frame
-			elif((((startX+endX)/2>(L*w)) and (((startX+endX)/2)<(R*w))))and((moveright==1)or(moveleft==1)):
-				mycam.ptz_stop_run()
-				moveright = 0
-				moveleft = 0
-			label = "{}: {:.2f}%".format(CLASSES[idx],
-				confidence * 100)
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				COLORS[idx], 2)
-			y = startY - 15 if startY - 15 > 15 else startY + 15
-			cv2.putText(frame, label, (startX, y),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+		(h, w) = frame.shape[:2]
+		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+			0.007843, (300, 300), 127.5)
+	#	print(w)
+		# pass the blob through the network and obtain the detections and
+		# predictions
+		net.setInput(blob)
+		detections = net.forward()
+		#print(frame.dtype)
+		# loop over the detections
+		for i in np.arange(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated with
+			# the prediction
+			confidence = detections[0, 0, i, 2]
+			idx2 = int(detections[0,0,i,1])
+			# filter out weak detections by ensuring the `confidence` is
+			# greater than the minimum confidence
+			if ((confidence > args["confidence"]) and (CLASSES[idx2]=='cat')):
+				# extract the index of the class label from the
+				# `detections`, then compute the (x, y)-coordinates of
+				# the bounding box for the object
+				idx = int(detections[0, 0, i, 1])
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
+			#	print('startX=',startX)
+			#	print('endX=',endX)
+				if(((startX+endX)/2<(L*w)) and (moveleft==0)):
+					mycam.ptz_move_left()
+					moveleft = 1
+					moveright = 0
+				#	canpoint = 0
+				#	pointat = time.time()+0.3 
+				elif(((startX+endX)/2>(R*w)) and (moveright==0)):
+					mycam.ptz_move_right()
+					moveright = 1
+					moveleft = 0
+				#	canpoint = 0
+				#	pointat = time.time()+0.3
+				# draw the prediction on the frame
+				elif((((startX+endX)/2>(L*w)) and (((startX+endX)/2)<(R*w))))and((moveright==1)or(moveleft==1)):
+					mycam.ptz_stop_run()
+					moveright = 0
+					moveleft = 0
+				label = "{}: {:.2f}%".format(CLASSES[idx],
+					confidence * 100)
+				cv2.rectangle(frame, (startX, startY), (endX, endY),
+					COLORS[idx], 2)
+				y = startY - 15 if startY - 15 > 15 else startY + 15
+				cv2.putText(frame, label, (startX, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
