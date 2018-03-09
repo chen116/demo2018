a11d82e4e006274e605479f4926c9bb29196f919
Victor Chen <yvictorck@gmail.com>
2018-03-09 01:11:38 -0800

    a

--- tf_track_vid_file_single.py
+++ tf_track_vid_file_single.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
     parser.add_argument('-ht', '--height', dest='height', type=int,
                         default=360, help='Height of the frames in the video stream.')
     parser.add_argument('-num-w', '--num-workers', dest='num_workers', type=int,
-                        default=1, help='Number of workers.')
+                        default=5, help='Number of workers.')
     parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
                         default=5, help='Size of the queue.')
     args = parser.parse_args()
