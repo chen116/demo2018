d6a7b30af03daa2dd172970c65f7abe66577f2f4
Victor Chen <yvictorck@gmail.com>
2018-03-08 18:53:12 -0800

    a

--- obj_track_vid_file.py
+++ obj_track_vid_file.py
@@ -61,14 +61,14 @@ mr.pack()
 moveright = 0
 moveleft = 0
 # construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
+ap = argparse.ArgumentParser()
 # ap.add_argument("-p", "--prototxt", required=True,
 # 	help="path to Caffe 'deploy' prototxt file")
 # ap.add_argument("-m", "--model", required=True,
 # 	help="path to Caffe pre-trained model")
-# ap.add_argument("-c", "--confidence", type=float, default=0.2,
-# 	help="minimum probability to filter weak detections")
-# args = vars(ap.parse_args())
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
 #os.system('python reset_cam.py') 
 #mycam.ptz_reset()
 # initialize the list of class labels MobileNet SSD was trained to
