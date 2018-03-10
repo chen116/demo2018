8d1db672264295bcf3c4debe40d9f14e626cb747
Victor Chen <yvictorck@gmail.com>
2018-03-10 15:49:50 -0800

    a

--- turn_cam_multi_threads.py
+++ turn_cam_multi_threads.py
@@ -278,7 +278,7 @@ while vs.more():
 
 		# show the output frame
 		cv2.imshow("Frame", frame)
-		if global_cnt>5:
+		if global_cnt>50:
 			# frameW = img_02.size[0]
 			# frameH = img_02.size[1]
 			outvid.write(frame)
