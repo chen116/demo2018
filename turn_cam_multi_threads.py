20bf3efddcd56d534a7d8635fa73b7ceac17bcb4
Victor Chen <yvictorck@gmail.com>
2018-03-10 15:26:20 -0800

    a

--- turn_cam_multi_threads.py
+++ turn_cam_multi_threads.py
@@ -170,7 +170,7 @@ prev_box = {}
 cnt=0
 global_cnt=0
 
-outvid = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (400,400))
+outvid = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (w1.get(),w1.get()))
 # while True:
 while vs.more():
 	# grab the frame from the threaded video stream and resize it
