fc1d78973a5213ef699c7f8b16b830ff7b8257c3
Victor Chen <yvictorck@gmail.com>
2018-03-10 16:36:36 -0800

    a

--- tf_track_vid_file_single_proc_multi_threads.py
+++ tf_track_vid_file_single_proc_multi_threads.py
@@ -171,8 +171,9 @@ input_q = Queue()  # fps is better if queue is higher but then more lags
 output_q = Queue()
 threads = []
 every_n_frame = {'cnt':-1,'n':5}
+total_num_threads=5
 threadLock = threading.Lock()
-for i in range(5):
+for i in range(total_num_threads):
     tmp_thread = Workers(threadLock,every_n_frame,PATH_TO_CKPT,i,input_q,output_q)
     tmp_thread.start()
     threads.append(tmp_thread)
