a90af5a9b2e337c0f042f11e8e4354971eb1af21
Victor Chen <yvictorck@gmail.com>
2018-03-10 19:37:53 -0800

    a

--- turn_cam_multi_threads.py
+++ turn_cam_multi_threads.py
@@ -82,13 +82,13 @@ c = Checkbutton(master, text="anchors", variable=checked)
 c.pack(side=LEFT)
 
 MODES = [
-    ("600", 600),
+    ("300", 300),
     ("800", 800),
     ("1000", 1000),
     ("done",0)
 ]
 w1 = IntVar()
-w1.set(600) # initialize
+w1.set(300) # initialize
 previous_f_size = w1.get()
 for text, mode in MODES:
     b = Radiobutton(master, text=text,variable=w1, value=mode)
@@ -170,11 +170,11 @@ prev_box = {}
 # loop over the frames from the video stream
 cnt=0
 global_cnt=0
-outvid = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (600,337))
+outvid = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (300,337))
 # while True:
 while vs.more():
 	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 600 pixels
+	# to have a maximum width of 300 pixels
 	frame = vs.read()
 	current_f_size=w1.get()
 	if current_f_size == 0:
