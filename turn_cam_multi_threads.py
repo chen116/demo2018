e1a2d136a69159546bba3a2592cade93db1594d0
Victor Chen <yvictorck@gmail.com>
2018-03-10 19:31:18 -0800

    a

--- turn_cam_multi_threads.py
+++ turn_cam_multi_threads.py
@@ -152,7 +152,7 @@ time.sleep(2.0)
 input_q = Queue()  # fps is better if queue is higher but then more lags
 output_q = Queue()
 threads = []
-every_n_frame = {'cnt':-1,'n':1}
+every_n_frame = {'cnt':-1,'n':5}
 threadLock = threading.Lock()
 total_num_threads=5
 num_threads_exiting=0
