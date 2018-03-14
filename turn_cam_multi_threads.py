dcff56fc2e17c597a435cfc70ee8b876f1d36ec3
Victor Chen <yvictorck@gmail.com>
2018-03-13 22:50:04 -0700

    a

--- turn_cam_multi_threads.py
+++ turn_cam_multi_threads.py
@@ -86,14 +86,22 @@ from tkinter import *
 import tkinter as tk
 
 master = tk.Tk()
-toplevel = tk.Toplevel(master)
-toplevel.update_idletasks()
-w = toplevel.winfo_screenwidth()
-h = toplevel.winfo_screenheight()
-size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
-x = w/2 - size[0]/2
-y = h/2 - size[1]/2
-toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
+# root = tk.Tk() # create a Tk root window
+
+w = 800 # width for the Tk root
+h = 650 # height for the Tk root
+
+# get screen width and height
+ws = root.winfo_screenwidth() # width of the screen
+hs = root.winfo_screenheight() # height of the screen
+
+# calculate x and y coordinates for the Tk root window
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+# set the dimensions of the screen 
+# and where it is placed
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 
 checked = IntVar(value=0)
