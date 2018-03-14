dff739ff12c6dfe0f0f807e75c419d2b0e1dcb45
Victor Chen <yvictorck@gmail.com>
2018-03-14 02:17:05 -0700

    a

--- object_rec_camera_pointing_v3.py
+++ object_rec_camera_pointing_v3.py
@@ -291,7 +291,7 @@ while vs.more(): # outvid
 				idx2 = int(detections[0,0,i,1])
 				# filter out weak detections by ensuring the `confidence` is
 				# greater than the minimum confidence
-				if ((confidence > 0.2) and (CLASSES[idx2]=='person')):
+				if ((confidence > 0.2) and (CLASSES[idx2]=='cat')):
 					# extract the index of the class label from the
 					# `detections`, then compute the (x, y)-coordinates of
 					# the bounding box for the object
