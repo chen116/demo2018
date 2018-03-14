746dd4edf253ad904205f88f82b145e07e6680c0
Victor Chen <yvictorck@gmail.com>
2018-03-13 22:50:35 -0700

    a

--- turn_cam_multi_threads.py
+++ turn_cam_multi_threads.py
@@ -85,15 +85,15 @@ from tkinter import *
 
 import tkinter as tk
 
-master = tk.Tk()
+master = Tk()
 # root = tk.Tk() # create a Tk root window
 
 w = 800 # width for the Tk root
 h = 650 # height for the Tk root
 
 # get screen width and height
-ws = root.winfo_screenwidth() # width of the screen
-hs = root.winfo_screenheight() # height of the screen
+ws = master.winfo_screenwidth() # width of the screen
+hs = master.winfo_screenheight() # height of the screen
 
 # calculate x and y coordinates for the Tk root window
 x = (ws/2) - (w/2)
