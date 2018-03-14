608b38ecf0c94707a0776693d9a4754cdcda9fd7
Victor Chen <yvictorck@gmail.com>
2018-03-13 22:53:41 -0700

    a

--- turn_cam_multi_threads.py
+++ turn_cam_multi_threads.py
@@ -97,7 +97,7 @@ hs = master.winfo_screenheight() # height of the screen
 
 # calculate x and y coordinates for the Tk root window
 x = (ws/2) - (w/2)
-y = (hs/2) - (h/2)
+y = (hs/2) - (h/4)
 
 # set the dimensions of the screen 
 # and where it is placed
