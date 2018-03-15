d8fad07440d62eebc8d84fdae3a0f13d1e854d54
Victor Chen <yvictorck@gmail.com>
2018-03-15 01:48:18 -0700

    a

--- multi_monitor.py
+++ multi_monitor.py
@@ -8,7 +8,7 @@ import threading
 import time
 import pprint
 monitoring_items = ["heart_rate","app_mode"]
-c = heartbeat.Dom0(monitoring_items,['2','3'])
+c = heartbeat.Dom0(monitoring_items,['4','5'])
 # c = heartbeat.Dom0(["heart_rate"])
 
 
