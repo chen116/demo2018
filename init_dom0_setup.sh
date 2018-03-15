f70838910eb4a9de92339fcc6488ca5418b1eb0f
Victor Chen <yvictorck@gmail.com>
2018-03-15 14:43:10 -0700

    a

--- init_dom0_setup.sh
+++ init_dom0_setup.sh
@@ -6,14 +6,18 @@ xl vcpu-pin 0 1 - 1
 xl vcpu-pin 0 2 - 2
 xl vcpu-pin 0 3 - 3
 xl vcpu-pin 0 4 - 4
-xl vcpu-pin 1 all 6-10
-xl vcpu-pin 3 all 6-10
+xl vcpu-pin 1 all 6-10 6-10
+xl vcpu-pin 3 all 6-10 6-10
 xl cpupool-migrate 2 credit
 xl cpupool-migrate 4 credit
-xl vcpu-pin 2 all 11-15
-xl vcpu-pin 4 all 11-15
+xl vcpu-pin 2 all 11-15 11-15
+xl vcpu-pin 4 all 11-15 11-15
 xl vcpu-list
 xl cpupool-list -c
 xl cpupool-list
 xl sched-credit
 xl sched-rtds -v all
+xl sched-rtds -d 1 -p 10000 -b 5000
+xl sched-rtds -d 3 -p 10000 -b 5000
+xl sched-credit -d 2 -w 5000 -c 500
+xl sched-credit -d 4 -w 5000 -c 500
