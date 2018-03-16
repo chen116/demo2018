41fcfa4a6fcd0b076ebc277f4ecca82fbcbd0a76
Victor Chen <yvictorck@gmail.com>
2018-03-15 18:11:41 -0700

    a

--- realtime_plot.py
+++ realtime_plot.py
@@ -1,8 +1,10 @@
+
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 
-fig = plt.figure()
+fig = plt.figure(figsize=(8, 6))
 ax1 = fig.add_subplot(2,1,1)
 ax2 = fig.add_subplot(2,1,2)
 buf = 1000
@@ -12,35 +14,88 @@ def animate(i):
     x = []
     hrs = []
     cpus = []
-    cnt=[]
     for i in range(2):
         x.append([])
         hrs.append([])
         cpus.append([])
-        cnt.append(-1)
-        for j in range(buf):
-            x[i].append(j)
-            hrs[i].append(0)
-            cpus[i].append(0)
+
+        # for j in range(buf):
+        #     x[i].append(j)
+        #     hrs[i].append(0)
+        #     cpus[i].append(0)
+    cnt=0
     for eachLine in dataArray:
         if len(eachLine)>1:
             line = eachLine.split()
             index=int(line[0])-1
-            cnt[index]+=1
-            hrs[index][cnt[index]]=(float(line[1]))
-            cpus[index][cnt[index]]=(float(line[2])/10000)
-    for i in range(2):
-        hrs[i]=hrs[i][:buf]
-        cpus[i]=cpus[i][:buf]
+            x[index].append(cnt)
+            hrs[index].append(float(line[1]))
+            cpus[index].append(float(line[2])/10000)
+            cnt+=1
 
     ax1.clear()
     ax2.clear()
-    opts=['r*','b*']
+    opts=['r*','bo']
+    sched=["RT-Xen","Credit"]
     for i in range(len(x)):
-        ax1.plot(x[i],hrs[i],opts[i])
-        ax2.plot(x[i],cpus[i],opts[i])
+        ax1.scatter(x[i],hrs[i],s=  ((i+1)%2)*6+5 ,label= sched[i]   )
+        ax2.scatter(x[i],cpus[i],s=  ((i+1)%2)*6+5,label= sched[i]   )
+    ax1.legend()
+    ax2.legend()
+    ax1.set_xlabel('time')
+    ax2.set_xlabel('time')
+    ax1.set_ylabel('Window FPS (Window Size = 5)')
+    ax2.set_ylabel('CPU Time Percentage')
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
+
+
+
+
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+# import time
+
+# fig = plt.figure()
+# ax1 = fig.add_subplot(2,1,1)
+# ax2 = fig.add_subplot(2,1,2)
+# buf = 1000
+# def animate(i):
+#     pullData = open("info.txt","r").read()
+#     dataArray = pullData.split('\n')
+#     x = []
+#     hrs = []
+#     cpus = []
+#     cnt=[]
+#     for i in range(2):
+#         x.append([])
+#         hrs.append([])
+#         cpus.append([])
+#         cnt.append(-1)
+#         for j in range(buf):
+#             x[i].append(j)
+#             hrs[i].append(0)
+#             cpus[i].append(0)
+#     for eachLine in dataArray:
+#         if len(eachLine)>1:
+#             line = eachLine.split()
+#             index=int(line[0])-1
+#             cnt[index]+=1
+#             hrs[index][cnt[index]]=(float(line[1]))
+#             cpus[index][cnt[index]]=(float(line[2])/10000)
+#     for i in range(2):
+#         hrs[i]=hrs[i][:buf]
+#         cpus[i]=cpus[i][:buf]
+
+#     ax1.clear()
+#     ax2.clear()
+#     opts=['r*','bo']
+#     for i in range(len(x)):
+#         ax1.plot(x[i],hrs[i],opts[i],markersize=((i+1)%2)*2+1)
+#         ax2.plot(x[i],cpus[i],opts[i],markersize=((i+1)%2)*3+1)
+
+# ani = animation.FuncAnimation(fig, animate, interval=1000)
+# plt.show()
 
 
