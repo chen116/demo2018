53e467ae33b968d9aa09e811fac47f7ba0ffe40d
Victor Chen <yvictorck@gmail.com>
2018-03-23 10:12:35 -0700

    a

--- realtime_plot.py
+++ realtime_plot.py
@@ -121,7 +121,7 @@ def animate2(i):
     # ax1.set_xlabel('Time\n \n')
     ax2.set_xlabel('Time')
     ax1.set_ylabel('Moving Average FPS(frames/sec) \n (Window Size = 5)')
-    ax2.set_ylabel('Assigned CPU Time Percentage (%)')
+    ax2.set_ylabel('Assigned CPU Time (%)')
     # ax2.set_ylim( 45, 105 )  
     ax2.set_ylim( -5, 105 )  
     ax=[ax1, ax2]
