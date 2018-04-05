8303ebc9f8c7ac3a13b51e0840f97691e7afaa2f
Victor Chen <yvictorck@gmail.com>
2018-04-05 16:14:42 -0700

    timeslices as options

--- gtPlot.py
+++ gtPlot.py
@@ -55,7 +55,7 @@ font_per = [{'family': 'serif',
 
 ax_cpu_saving = plt.axes([0.62, 0.91, 0.2, 0.12])
 ax_cpu_saving.text(0.06,0.42,'Cumulative RT-Xen CPU Utilization Savings:',fontdict=font_per[0])
-ax_cpu_saving_txt = ax_cpu_saving.text(.73,0.01,'%.2f%%'%(0),fontdict=font_per[1])
+ax_cpu_saving_txt = ax_cpu_saving.text(.8,0.01,'%.2f%%'%(0),fontdict=font_per[1])
 ax_cpu_saving.axis('off')
 
 def animate(frame):
