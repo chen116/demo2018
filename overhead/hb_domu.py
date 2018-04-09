b957244c89ba3270951d2575ea9a53d55e8a64a4
Victor Chen <yvictorck@gmail.com>
2018-04-09 12:23:07 -0700

    timeslices as options

--- overhead/hb_domu.py
+++ overhead/hb_domu.py
@@ -7,14 +7,14 @@ import heartbeat
 window_size_hr=5
 hb = heartbeat.Heartbeat(1024,window_size_hr,10000,"vic.log",10,100)
 #             shm_key, win_size,buf_depth,log_file,min_target,max_target):
-# monitoring_items = ["heart_rate","app_mode","frame_size","timeslice"]
-# comm = heartbeat.DomU(monitoring_items)
+monitoring_items = ["heart_rate","app_mode","frame_size","timeslice"]
+comm = heartbeat.DomU(monitoring_items)
 print("start")
 for i in range(100000):
 # hb stuff
 	hb.heartbeat_beat()
-	# window_hr = hb.get_window_heartrate()
-	# comm.write("heart_rate",window_hr)
+	window_hr = hb.get_window_heartrate()
+	comm.write("heart_rate",window_hr)
 # #print("hb: before get_instant_heartrate()")
 print(hb.get_instant_heartrate())
 # #print("hb: after hb stuff")
