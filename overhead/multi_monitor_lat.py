a200763cfdf057b8394940f30014424d20495eb4
Victor Chen <yvictorck@gmail.com>
2018-04-10 12:55:48 -0700

    timeslices as options

--- overhead/multi_monitor_lat.py
+++ overhead/multi_monitor_lat.py
@@ -65,14 +65,15 @@ class MonitorThread(threading.Thread):
 						heart_rate=-1
 					if heart_rate>-1:
 						# c.write(self.write_tmp_key_path,msg)
-						
+
 						success = False
 						while not success:
 							c.transaction()
 							c.write(self.write_tmp_key_path,msg)
 							success = c.commit()
-						print(int(msg.decode()))
 						self.ovh_cnt=1
+
+						# print(int(msg.decode()))
 						
 				else:
 					success = False
