e4fddb33fef3909a4e67cecf9a305ce2f643fdd4
Victor Chen <yvictorck@gmail.com>
2018-04-10 10:18:50 -0700

    timeslices as options

--- overhead/hb_domu.py
+++ overhead/hb_domu.py
@@ -23,12 +23,12 @@ lat_or_thruput=sys.argv[1]
 # thruput
 if 'thru' in lat_or_thruput:
 	window_size_hr=5
-	hb = heartbeat.Heartbeat(1024,window_size_hr,10000,"vic.log",10,10000)
+	hb = heartbeat.Heartbeat(1024,window_size_hr,100,"vic.log",10,100)
 	#             shm_key, win_size,buf_depth,log_file,min_target,max_target):
 	monitoring_items = ["heart_rate","app_mode"]
 	comm = heartbeat.DomU(monitoring_items)
 	print("start")
-	for i in range(10000-1):
+	for i in range(100-1):
 	# hb stuff
 		hb.heartbeat_beat()
 		# window_hr = hb.get_window_heartrate()
