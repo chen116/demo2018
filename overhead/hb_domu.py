031f96132e30d9da46f33486dd98e11c99b02557
Victor Chen <yvictorck@gmail.com>
2018-04-11 13:15:47 -0700

    timeslices as options

--- overhead/hb_domu.py
+++ overhead/hb_domu.py
@@ -37,8 +37,8 @@ if 'thru' in lat_or_thruput:
 		hb.heartbeat_beat()
 		comm.write("heart_rate",i)
 
-	# hb.heartbeat_beat()
-	# comm.write("heart_rate","reset")
+	hb.heartbeat_beat()
+	comm.write("heart_rate","reset")
 
 	# a= np.random.rand(500, 500)
 	# b= np.random.rand(500,500)
