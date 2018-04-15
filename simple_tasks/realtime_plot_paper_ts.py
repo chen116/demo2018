41b6964b4d4cfa2cf196c332abb0992c39d3b432
Victor Chen <yvictorck@gmail.com>
2018-04-15 14:03:36 -0700

    timeslices as options

--- simple_tasks/realtime_plot_paper_ts.py
+++ simple_tasks/realtime_plot_paper_ts.py
@@ -141,7 +141,7 @@ def animate2(i):
         miny.append(min_max[0])
         maxy.append(min_max[1])
     ax1.plot(x_for_minmax,miny,'r')
-    # ax1.plot(x_for_minmax,maxy,'r',label= 'Target\nFPS\nInterval')
+    ax1.plot(x_for_minmax,maxy,'r',label= 'Target\nFPS\nInterval')
     fontP = FontProperties()
     fontP.set_size('small')
     ax1.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.,prop=fontP)
