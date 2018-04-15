e8261c6b4c10b3d3bb255e5bb9e7f31a6b90cd7e
Victor Chen <yvictorck@gmail.com>
2018-04-15 16:43:49 -0700

    timeslices as options

--- simple_tasks/mat_mul.py
+++ simple_tasks/mat_mul.py
@@ -8,7 +8,7 @@ import numpy as np
 window_size_hr=5
 hb = heartbeat.Heartbeat(1024,window_size_hr,1000,"vic.log",10,1000)
 # #            shm_key, win_size,buf_depth,log_file,min_target,max_target):
-monitoring_items = ["heart_rate","app_mode"]
+monitoring_items = ["heart_rate","app_mode","frame_size"]
 comm = heartbeat.DomU(monitoring_items)
 # print("start")
 st = time.time()
@@ -25,10 +25,10 @@ b=np.asarray(a)
 # a= np.random.rand(500, 500)
 # b= np.random.rand(500, 500)	
 
-it = 
+it = 500
 matsize = 500
 try:
-	for i in range(it*3):
+	for i in range(it*6):
 	# hb stuff
 		a= np.random.rand(matsize, matsize)
 		b= np.random.rand(matsize, matsize)	
@@ -42,13 +42,33 @@ try:
 		if i%window_size_hr==0:
 			comm.write("heart_rate", hb.get_window_heartrate())
 		if i==it*1:
+			matsize=600
+			comm.write("frame_size", matsize)
+
+		elif i==it*2:
 			comm.write("app_mode", 2)
-		if i==it*1+10:
+			matsize=500
+			comm.write("frame_size", matsize)
+
+		elif i==it*2+10:
 			comm.write("app_mode", 3)
-		if i==it*2:
+		elif i==it*3:
+			matsize=600
+			comm.write("frame_size", matsize)
+
+
+		elif i==it*4:
 			comm.write("app_mode", 2)
-		if i==it*2+10:
+			matsize=500
+			comm.write("frame_size", matsize)
+
+
+		elif i==it*4+10:
 			comm.write("app_mode", 4)
+		elif i==it*5:
+			matsize=600
+			comm.write("frame_size", matsize)
+			
 
 
 except:
