efc6f4f0d680fb62a6d5e560096044192c4dfcc9
Victor Chen <yvictorck@gmail.com>
2018-04-16 14:19:20 -0700

    timeslices as options

--- object_rec_camera_pointing_v3_ts_paper.py
+++ object_rec_camera_pointing_v3_ts_paper.py
@@ -484,8 +484,8 @@ while True: # realvid
 			# #print("hb: before get_instant_heartrate()")
 			# instant_hr = hb.get_instant_heartrate()
 			# #print("hb: after hb stuff")
-			if output_q_cnt>window_size_hr: #and output_q_cnt%m1.get()==0:
-				comm.write("heart_rate",hb.get_instant_heartrate())
+			if output_q_cnt>window_size_hr and output_q_cnt%m1.get()==0:
+				comm.write("heart_rate",hb.get_window_heartrate())
 			# #print('------------------window_hr:',window_hr)
 			# #print('instant_hr:',instant_hr)
 			current_checked = checked.get()
