60dbf50ca08fea5420978f5357800c3e6b89fb0c
Victor Chen <yvictorck@gmail.com>
2018-04-16 12:00:55 -0700

    timeslices as options

--- object_rec_camera_pointing_v3_ts_paper.py
+++ object_rec_camera_pointing_v3_ts_paper.py
@@ -306,8 +306,8 @@ pointat = 0
 # loop over the frames from the video stream
 
 prev_personincam = personincam
-while vs.more(): # outvid
-# while True: # realvid
+# while vs.more(): # outvid
+while True: # realvid
 
 	# frame = vs.read()
 	frame = cat_frame # outvid
