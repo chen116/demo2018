d2a010a7b336ca61bd8b2e630f8c96e5205db1d8
Victor Chen <yvictorck@gmail.com>
2018-04-18 07:35:52 -0700

    timeslices as options

--- object_rec_camera_pointing_v3_ts_paper.py
+++ object_rec_camera_pointing_v3_ts_paper.py
@@ -224,7 +224,8 @@ m1.set(5)
 threads = []
 every_n_frame = {'cnt':-1,'n':w1.get()}
 threadLock = threading.Lock()
-total_num_threads = 3
+total_num_threads = 4 # realvid
+# total_num_threads = 4 # fastcat
 num_threads_exiting = 0
 
 
