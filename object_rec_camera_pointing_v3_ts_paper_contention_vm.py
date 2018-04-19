e384f52dca150f5923d624fa96e651078c9e0d8e
Victor Chen <yvictorck@gmail.com>
2018-04-19 12:23:08 -0700

    timeslices as options

--- object_rec_camera_pointing_v3_ts_paper_contention_vm.py
+++ object_rec_camera_pointing_v3_ts_paper_contention_vm.py
@@ -459,7 +459,7 @@ for frame in vidarray: # fastcat
 
 
 		current_freq=w1.get()
-		if remotetrack == -1 or w1.get() == 0 or output_q_cnt== onecatvidlen*3:
+		if remotetrack == -1 or w1.get() == 0:
 			threadLock.acquire()
 			every_n_frame['n']=-1
 			threadLock.release()
