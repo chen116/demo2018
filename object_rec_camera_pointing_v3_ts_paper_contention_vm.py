ec776fa3c397c820101a46af6e1909c62b40cca1
Victor Chen <yvictorck@gmail.com>
2018-04-20 06:31:04 -0700

    timeslices as options

--- object_rec_camera_pointing_v3_ts_paper_contention_vm.py
+++ object_rec_camera_pointing_v3_ts_paper_contention_vm.py
@@ -389,10 +389,10 @@ if catlen>0:
 
 	if "RT" in sys.argv[7]:
 		vidarray = np.concatenate((car_vidarray[0:len(car_vidarray/2)],car_vidarray,cat_vidarray,cat_vidarray,man_vidarray,car_vidarray),axis=0)
-		vidarray = np.concatenate((cat,car,car),axis=0)
+		vidarray = np.concatenate((cat,man,car),axis=0)
 	else:
 		vidarray = np.concatenate((car_vidarray,cat_vidarray,man_vidarray,man_vidarray,car_vidarray,car_vidarray[0:len(car_vidarray/2)]),axis=0)
-		vidarray = np.concatenate((cat,cat,car),axis=0)
+		vidarray = np.concatenate((car,car,cat),axis=0)
 	print(len(vidarray))
 
 
