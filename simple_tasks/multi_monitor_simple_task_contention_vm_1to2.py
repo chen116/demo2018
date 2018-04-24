c3858f65bdeb1166f66c3afbe4d7b559cfb39cac
Victor Chen <yvictorck@gmail.com>
2018-04-24 14:52:30 -0700

    timeslices as options

--- simple_tasks/multi_monitor_simple_task_contention_vm_1to2.py
+++ simple_tasks/multi_monitor_simple_task_contention_vm_1to2.py
@@ -317,7 +317,7 @@ class MonitorThread(threading.Thread):
 				cur_bw=self.timeslice_us-other_cur_bw
 				self.pid.reset()
 
-			process_unit_time=3
+			process_unit_time=2
 			if self.shared_data["contention_time_passed"]>=process_unit_time:# and int(self.shared_data["contention_time_passed"])%5==0:
 				self.shared_data["contention_time_passed"]=0
 				if my_pass_val<=other_pass_val:
